50f2d6386acbe55e8a3ccb4c0573733db4282ed7
Olesya Safronova <olesyabos@mail.ru>
2022-09-20 20:59:11 +0300

    PrintArray

--- Work/Program.cs
+++ Work/Program.cs
@@ -55,3 +55,15 @@ void FillNewArray(string[] oldArray, string[] newArray, int lengthLimit)
     }
 }
 
+string PrintArray(string[] array)
+{
+    string result = string.Empty;
+    result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {	
+        result += $"{array[i],1}";
+        if (i < array.Length - 1) result += ", ";
+    }
+    result += " ]";
+    return result;
+}
